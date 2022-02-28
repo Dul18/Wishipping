24a76e0eab150f67218edcfdd4d58d3c3d6672d7
dulankaj <dulankaj@emojot.com>
2022-02-28 18:19:06 +0530

    01-Create React app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">ShoppingApp</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
