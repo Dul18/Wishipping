027f017a9465541c1ae01c7d2d64533f7059ee97
dulankaj <dulankaj@emojot.com>
2022-04-08 16:48:12 +0530

    20-Create Signin Backend API

--- README.md
+++ README.md
@@ -113,3 +113,8 @@
 19. Seed Sample Users
     1. create user model
     2. seed sample users
+
+20. Create Signin Backend API
+    1. create signin api
+    2. npm install jsonwebtoken
+    3. define generateToken
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js'
+import userRouter from './routes/userRoutes.js';
+import expressAsyncHandler from 'express-async-handler';
 
 dotenv.config();
 mongoose.connect(process.env.MONGODB_URI).then(()=>{
@@ -13,8 +15,17 @@ mongoose.connect(process.env.MONGODB_URI).then(()=>{
 })
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
+app.use('/api/users', userRouter)
+
+app.use((err,req,res,next)=>{
+  res.status(500).send({message:err.message});
+})
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
